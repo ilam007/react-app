efb4b5f68a57d95d5276e4c9c185281bdbe5670e
ilam007 <ilamparthe005@gmail.com>
2024-01-24 16:05:25 +0530

    main

---
+++ KEC/react-app/src/components/functionalComponents/testComponent.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Login from "../classComponents/LoginComponent";
+
+function Testcomp(){
+    return(
+        <div>
+            <h1>This is a test Functional Component.</h1>
+            <Login></Login>
+        </div>
+    );
+}
+
+/*function Testcomp(){
+    return(
+        <div>
+            <h2>WELCOME TO REACT APP!!</h2>
+            <h1>This is a test Functional Component.</h1>
+        </div>
+    );
+}*/
+
+export default Testcomp;
+
+
+
+/*
+function add(a,b){
+    return a+b
+}
+console.log(add(2,5))*/
